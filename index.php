3396f011f254909088af8dc0e89d44b7de7fb8bf
JhanmaicolJesus <69094652+JhanmaicolJesus@users.noreply.github.com>
2020-08-02 21:57:06 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>SpinOuT World</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header class="full-width NavBarP"></header>
+	<section class="banner">
+		<div class="banner-content">
+			<h1>SpinOuT World</h1>
+		</div>
+	</section>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
