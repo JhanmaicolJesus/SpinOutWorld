871f9e3034f28d8f77bb5cea1720e57181c2717f
JhanmaicolJesus <69094652+JhanmaicolJesus@users.noreply.github.com>
2020-08-03 00:59:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>SpinOuT World</title>
+	<title>SpinOut World</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<header class="full-width NavBarP"></header>
 	<section class="banner">
 		<div class="banner-content">
-			<h1>SpinOuT World</h1>
+			<h1>SpinOut World</h1>
 		</div>
 	</section>
 	<script src="js/bootstrap.min.js"></script>
